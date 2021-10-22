bee87225d86868965e05fa4f3a3ebe1463bfafdb
Ayshahananv <92805307+Ayshahananv@users.noreply.github.com>
2021-10-22 07:38:45 -0400

    Add files via upload

---
+++ Book Store Application/Book Store Application/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Book_Store_Application.Models;
+
+namespace Book_Store_Application.Controllers
+{
+    public class UsersController : ApiController
+    {
+        Users userobj = new Users();
+        public List<Users> GetAllUsers()
+        {
+            return userobj.GetAllUsers();
+        }
+        public Users GetUserById(int p_userId)
+        {
+            return userobj.GetUserById(p_userId);
+        }
+        [HttpPost]
+        public List<Users> Post(Users newobj)
+        {
+            userobj.addUser(newobj);
+            return userobj.GetAllUsers();
+        }
+        [HttpPut]
+        public List<Users> Put(Users newuser)
+        {
+            userobj.updateUser(newuser);
+            return userobj.GetAllUsers();
+        }
+        [HttpDelete]
+        public List<Users> Delete(int p_userId)
+        {
+            userobj.deleteUser(p_userId);
+            return userobj.GetAllUsers();
+        }
+    }
+}
