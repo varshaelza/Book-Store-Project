c4888f8609859c6ef632805ce5fa4177067a531d
Varsha Elza <43822242+varshaelza@users.noreply.github.com>
2021-10-25 09:02:12 +0530

    Update PurchasesController.cs

--- Book Store Application/Book Store Application/Controllers/PurchasesController.cs
+++ Book Store Application/Book Store Application/Controllers/PurchasesController.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.IO;
 using Book_Store_Application.Models;
 
 namespace Book_Store_Application.Controllers
@@ -13,21 +14,58 @@ namespace Book_Store_Application.Controllers
         public List<Purchases> GetAllPurchases()
         {
             Purchases purchaseObj = new Purchases();
-            return purchaseObj.GetAllPurchases();
+            List<Purchases> purchaseList = new List<Purchases>();
+            try
+            {
+                purchaseList= purchaseObj.GetAllPurchases();
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(fs);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                fs.Close();
+            }
+            return purchaseList;
         }
 
         [HttpGet]
         public List<Purchases> GetPurchaseByOrderId(int p_orderId)
         {
             Purchases purchaseObj = new Purchases();
-            return purchaseObj.GetPurchasebyOrderId(p_orderId);
+            List<Purchases> purchaseList = new List<Purchases>();
+            try
+            {
+                purchaseList = purchaseObj.GetPurchasebyOrderId(p_orderId);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(fs);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                fs.Close();
+            }
+            return purchaseList;
         }
 
         [HttpPost]
         public List<Purchases> PostPurchase(Purchases p_purchaseObj)
         {
             Purchases purchaseObj = new Purchases();
-            purchaseObj.AddPurchase(p_purchaseObj);
+            try
+            {
+                int rowsAffected = purchaseObj.AddPurchase(p_purchaseObj);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(fs);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                fs.Close();
+            }
             return purchaseObj.GetAllPurchases();
         }
 
@@ -35,15 +73,37 @@ namespace Book_Store_Application.Controllers
         public List<Purchases> PutPurchase(Purchases p_purchaseObj)
         {
             Purchases purchaseObj = new Purchases();
-            purchaseObj.UpdatePurchase(p_purchaseObj);
+            try
+            {
+                int rowsAffected = purchaseObj.UpdatePurchase(p_purchaseObj);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(fs);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                fs.Close();
+            }
             return purchaseObj.GetAllPurchases();
         }
 
         [HttpDelete]
-        public List<Purchases> DeletePurchase(Purchases p_purchaseObj)
+        public List<Purchases> DeletePurchase(int p_purchaseId)
         {
             Purchases purchaseObj = new Purchases();
-            purchaseObj.DeletePurchase(p_purchaseObj.purchaseId);
+            try
+            {
+                int rowsAffected = purchaseObj.DeletePurchase(p_purchaseId);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(fs);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                fs.Close();
+            }
             return purchaseObj.GetAllPurchases();
         }
     }
