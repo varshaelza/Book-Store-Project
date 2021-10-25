d310774fad2fbcbbebdd5d293b5044b047c1c154
divya1999ann <divya.kurien@gmail.com>
2021-10-25 15:01:02 -0400

    Update OrderController.cs

--- Book Store Application/Book Store Application/Controllers/OrderController.cs
+++ Book Store Application/Book Store Application/Controllers/OrderController.cs
@@ -4,29 +4,72 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.IO;
 using Book_Store_Project.Models;
 namespace Book_Store_Project.Controllers
 {
     
     public class OrderController : ApiController
     {
-        Orders ord = new Orders();
+        
+
         [HttpGet]        
         public List<Orders> GetOrders()
         {
-            return ord.GetAllOrders();
+            List<Orders> ordList = new List<Orders>();
+            try
+            {
+                Orders ord = new Orders();
+                ordList = ord.GetAllOrders();
+            }
+            catch(Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(ex.Message);
+                sw.Close();
+                fs.Close();
+            }
+            return ordList;
         }
 
         [HttpGet]
         public List<Orders> GetByUserId(int userId)
         {
+            List<Orders> ordList = new List<Orders>();
+            try
+            {
+                Orders ord = new Orders();
+                ordList = ord.GetAllOrdersById(userId);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(ex.Message);
+                sw.Close();
+                fs.Close();
+            }
+            return ordList;
 
-            return ord.GetAllOrdersById(userId);
         }
+
         [HttpPost]
         public List<Orders> PostOrder(Orders newObj)
         {
-            ord.AddOrder(newObj);
+            Orders ord = new Orders();
+            try
+            {
+                ord.AddOrder(newObj);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(ex.Message);
+                sw.Close();
+                fs.Close();
+            }
             return ord.GetAllOrders();
 
         }
