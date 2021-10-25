7b8894340a8bbbda73c9230de03ccc68556bbc4b
divya1999ann <divya.kurien@gmail.com>
2021-10-25 14:59:51 -0400

    Update CartController.cs

--- Book Store Application/Book Store Application/Controllers/CartController.cs
+++ Book Store Application/Book Store Application/Controllers/CartController.cs
@@ -4,42 +4,109 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.IO;
 using Book_Store_Project.Models;
 namespace Book_Store_Project.Controllers
 {
     public class CartController : ApiController
     {
-        Cart cart = new Cart();
+        
 
         [HttpGet]
         public List<Cart> GetCart()
         {
-            return cart.GetAllItems();
+            List<Cart> cartList = new List<Cart>();
+            try
+            {
+                Cart cart = new Cart();
+                cartList = cart.GetAllItems();
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(ex.Message);
+                sw.Close();
+                fs.Close();
+            }
+            return cartList;
         }
+
         [HttpGet]
         public List<Cart> GetCart(int userId)
         {
-            
-            return cart.GetbyUserId(userId); 
+            List<Cart> cartList = new List<Cart>();
+            try
+            {
+                Cart cart = new Cart();
+                cartList = cart.GetbyUserId(userId);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(ex.Message);
+                sw.Close();
+                fs.Close();
+            }
+            return cartList;
         }
 
-        [System.Web.Http.HttpPost]
+        [HttpPost]
         public List<Cart> PostCart(Cart newObj)
         {
-            cart.AddItem(newObj);
+            Cart cart = new Cart();
+            try
+            {
+                cart.AddItem(newObj);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(ex.Message);
+                sw.Close();
+                fs.Close();
+            }
             return cart.GetAllItems();
   
         }
         [HttpPut]
         public List<Cart> PutCart(int id, Cart updObj)
         {
-            cart.UpdateItem(id, updObj);
+            Cart cart = new Cart();
+            try
+            {
+                cart.UpdateItem(id, updObj);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(ex.Message);
+                sw.Close();
+                fs.Close();
+            }
+            
             return cart.GetAllItems();
         }
         [HttpDelete]
         public List<Cart> DeleteCart(int id)
         {
-            cart.DeleteItem(id);
+            Cart cart = new Cart();
+            try
+            {
+                cart.DeleteItem(id);
+            }
+            catch (Exception ex)
+            {
+                FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(ex.Message);
+                sw.Close();
+                fs.Close();
+            }
+            
             return cart.GetAllItems();
         }
     }
