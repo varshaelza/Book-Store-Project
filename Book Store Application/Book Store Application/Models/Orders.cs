c8a4ffae98d5fe6c79f7736575bea5b6ced6a0d5
divya1999ann <divya.kurien@gmail.com>
2021-10-25 15:03:25 -0400

    Update Orders.cs

--- Book Store Application/Book Store Application/Models/Orders.cs
+++ Book Store Application/Book Store Application/Models/Orders.cs
@@ -20,15 +20,15 @@ namespace Book_Store_Project.Models
         SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connectBookStoreDB"].ConnectionString);
 
         SqlCommand cmd_getAllOrders = new SqlCommand("select * from Orders");
-        SqlCommand cmd_getAllOrdersByOrderId = new SqlCommand("select userId, couponId, totalAmt, dateTimeOrder from Orders where orderId=@orderId");
+        SqlCommand cmd_getAllOrdersByOrderId = new SqlCommand("select * from Orders where orderId=@orderId");
         SqlCommand cmd_getAllOrdersByUserId = new SqlCommand("select * from Orders where userId=@userId");
         SqlCommand cmd_addOrder = new SqlCommand("insert into Orders values(@userId, @couponId, @totalAmt, @dateTimeOrder)");
 
-        List<Orders> ordList = new List<Orders>();
 
         #region Methods
         public List<Orders> GetAllOrders()
         {
+            List<Orders> ordList = new List<Orders>();
             cmd_getAllOrders.Connection = con; //my command is going to use connection
             SqlDataReader _read;
             con.Open();
@@ -47,11 +47,16 @@ namespace Book_Store_Project.Models
             _read.Close();
             con.Close();
 
+            if (ordList.Count == 0)
+            {
+                throw new Exception("Orders table does not contain any entries");
+            }
             return ordList;
         }
 
         public List<Orders> GetAllOrdersByOrderId(int id)
         {
+            List<Orders> ordList = new List<Orders>();
             cmd_getAllOrdersByOrderId.Connection = con; //my command is going to use connection
             cmd_getAllOrdersByOrderId.Parameters.AddWithValue("@orderId", id);
             SqlDataReader _read;
@@ -61,20 +66,27 @@ namespace Book_Store_Project.Models
             {
                 ordList.Add(new Orders()
                 {
-                    userId = Convert.ToInt32(_read[0]),
-                    couponId = Convert.ToInt32(_read[1]),
-                    totalAmt = Convert.ToDouble(_read[2]),
-                    dateTimeOrder = Convert.ToDateTime(_read[3])
+                    orderId = Convert.ToInt32(_read[0]),
+                    userId = Convert.ToInt32(_read[1]),
+                    couponId = Convert.ToInt32(_read[2]),
+                    totalAmt = Convert.ToDouble(_read[3]),
+                    dateTimeOrder = Convert.ToDateTime(_read[4])
                 });
             }
             _read.Close();
             con.Close();
+
+            if (ordList.Count == 0)
+            {
+                throw new Exception("Record orderId = " + id + " not found in Orders table");
+            }
 
             return ordList;
         }
 
         public List<Orders> GetAllOrdersById(int id)
         {
+            List<Orders> ordList = new List<Orders>();
             cmd_getAllOrdersByUserId.Connection = con; //my command is going to use connection
             cmd_getAllOrdersByUserId.Parameters.AddWithValue("@userId", id);
             SqlDataReader _read;
@@ -94,6 +106,10 @@ namespace Book_Store_Project.Models
             _read.Close();
             con.Close();
 
+            if (ordList.Count == 0)
+            {
+                throw new Exception("Record userId = " + id + " not found in Orders table");
+            }
             return ordList;
         }
 
@@ -110,6 +126,11 @@ namespace Book_Store_Project.Models
             con.Open();
             int result = cmd_addOrder.ExecuteNonQuery();
             con.Close();
+
+            if (result == 0)
+            {
+                throw new Exception("Could not add entry into Orders table");
+            }
             return result;
 
         }
