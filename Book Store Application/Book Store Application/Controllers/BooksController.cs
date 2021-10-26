9e1243516f012051ef2ae8ad5944f146a4a20494
Ayshahananv <92805307+Ayshahananv@users.noreply.github.com>
2021-10-26 09:47:11 -0400

    Update BooksController.cs

--- Book Store Application/Book Store Application/Controllers/BooksController.cs
+++ Book Store Application/Book Store Application/Controllers/BooksController.cs
@@ -5,42 +5,123 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Book_Store_Application.Models;
+using System.IO;
 
 namespace Book_Store_Application.Controllers
 {
     public class BooksController : ApiController
     {
+        List<Books> booklist = new List<Books>();
         Books bookObj = new Books();
 
         public List<Books> GetAllBooks()
         {
-            return bookObj.GetAllBooks();
+
+            try
+            {                
+                booklist = bookObj.GetAllBooks();
+            }
+            catch (Exception ex)
+            {
+                FileStream myfile = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(myfile);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                myfile.Close();
+
+            }
+            return booklist;
         }
         public List<Books> GetBookByCategory(int p_catID)
         {
-            return bookObj.GetBookByCategory(p_catID);
+            try
+            {
+                booklist = bookObj.GetBookByCategory(p_catID);
+            }
+            catch (Exception ex)
+            {
+                FileStream myfile = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(myfile);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                myfile.Close();
+
+            }
+            return booklist;
         }
 
         public List<Books> GetBookByTitleAuthor(string p_value)
         {
-            return bookObj.GetBookByTitleAuthor(p_value);
+            try
+            {
+                booklist = bookObj.GetBookByTitleAuthor(p_value);
+            }
+            catch (Exception ex)
+            {
+                FileStream myfile = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(myfile);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                myfile.Close();
+
+            }
+            return booklist;
         }
         [HttpPost]
         public List<Books> Post(Books newobj)
         {
-            bookObj.addBook(newobj);
+            try
+            {
+                bookObj.addBook(newobj);
+            }
+            catch (Exception ex)
+            {
+                FileStream myfile = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(myfile);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                myfile.Close();
+
+            }
+            
             return bookObj.GetAllBooks();
         }
         [HttpPut]
         public List<Books> Put(Books newbuk)
         {
-            bookObj.updateBook(newbuk);
+            try
+            {
+                bookObj.updateBook(newbuk);
+            }
+            catch (Exception ex)
+            {
+                FileStream myfile = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(myfile);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                myfile.Close();
+
+            }
+
             return bookObj.GetAllBooks();
         }
         [HttpDelete]
         public List<Books> Delete(int p_bookID)
         {
-            bookObj.deleteBook(p_bookID);
+            try
+            {
+                bookObj.deleteBook(p_bookID);
+            }
+            catch (Exception ex)
+            {
+                FileStream myfile = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "errorlog.txt"), FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(myfile);
+                sr.WriteLine(ex.Message);
+                sr.Close();
+                myfile.Close();
+
+            }
+
             return bookObj.GetAllBooks();
         }
 
